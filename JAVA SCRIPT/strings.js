cdb5f740a3118ec54a48918156acd67015f52ef9
malik09-teach <malikabbaskhan477@gmail.com>
2026-04-21 17:38:47 +0500

    upt

---
+++ JAVA SCRIPT/strings.js
@@ -0,0 +1,32 @@
+console.log("string opreations");
+let a = "abbas";
+let b = "MALIK";
+
+
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+
+
+console.log("printing by loop");
+
+
+for (let i=0 ;i<5;i++){
+
+console.log(a[i]);
+
+}
+
+
+console.log( "the functions operations");
+
+console.log(a.toUpperCase());
+console.log(b.toLowerCase());
+
+console.log(b.concat(a.toUpperCase()));
+
+console.log(a.slice(1,3));
+console.log(a.slice(1));
+
