ea78c57839a3c07900fbd04bad579c685c2f2a04
malik09-teach <malikabbaskhan477@gmail.com>
2026-04-28 15:52:17 +0500

    DOM WITH CSS

---
+++ DOM/script.js
@@ -0,0 +1,28 @@
+// get by index and get index 
+/*
+let box=document.body.getElementsByClassName("box");
+box[0].style.backgroundColor="lime"
+
+box[1].style.backgroundColor="brown"
+
+box[2].style.backgroundColor="yellow"
+
+box[3].style.backgroundColor="pink"
+
+box[4].style.backgroundColor="green"
+
+use in the static elements 
+if we add the  element then it will apply on the same index 
+
+
+*/
+
+
+
+//  by get id 
+
+
+
+
+
+document.getElementById("box1").style.backgroundColor ="red"
