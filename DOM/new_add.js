fb4233c076ab11e9516ef0f011993de38ff97e21
malik09-teach <malikabbaskhan477@gmail.com>
2026-04-28 16:58:53 +0500

    upt

---
+++ DOM/new_add.js
@@ -0,0 +1,11 @@
+let div=document.createElement("div")
+
+
+div.externlHTML="i am  working as dev <b> MALIK </b>"
+
+
+div.setAttribute("class","created")
+
+
+
+document.querrySelector("./container").append(div)
