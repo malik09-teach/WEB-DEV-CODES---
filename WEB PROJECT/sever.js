a07ff5c10b82a4c0e97f1688629ce3b9af1c359c
malik09-teach <malikabbaskhan477@gmail.com>
2026-05-09 15:23:04 +0500

    server running

--- WEB PROJECT/sever.js
+++ WEB PROJECT/sever.js
@@ -1,4 +1,17 @@
-import { slugify } from "slugify";
 
-console.log("hello world");
-var slug=slugify("something-world");
+// 1. Import built-in and custom modules
+const http = require('http');
+const addNumbers = require('./math');
+
+// 2. Create the server logic
+const server = http.createServer((req, res) => {
+    const result = addNumbers(110, 5);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(`Hello! The sum of 100 and 5 is ${result}`);
+});
+
+// 3. Tell the server to listen on a port
+server.listen(5000, 'localhost', () => {
+    console.log('Server is running at http://localhost:5000/');
+});
